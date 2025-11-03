f846131d1a911abe8579db58924ccf77e141c33e
Rob Hess <hessro@oregonstate.edu>
2025-11-03 15:44:33 -0800

    Add code from 11/3 lecture.

--- words.js
+++ words.js
@@ -10,11 +10,42 @@ var counter = 0
  * The globals `allWords` and `counter` are updated by this function.
  */
 function handleNewWordsEntered(event) {
-  var input = ''
+  console.log("== handleNewWordsEntered() called")
+  var input = event.currentTarget.value
+  console.log("== handleNewWordsEntered() called, input:", input)
   allWords = input.replace(/[!"#$%&\\'()\*+,\-\.\/:;<=>?@\[\\\]\^_`{|}~]/g, '')
     .toLowerCase()
     .split(' ')
   counter = 0
+  console.log(" -- allWords:", allWords)
+}
+
+var wordsInput = document.getElementById("words-input")
+wordsInput.addEventListener("change", handleNewWordsEntered)
+
+function getHighlightColor() {
+  return null
+}
+
+function handleAddWordButtonClick(event) {
+  console.log("== add word button clicked")
+  var wordIdx = counter % allWords.length
+  var word = allWords[wordIdx]
+  if (word) {
+    console.log(" -- word:", word)
+    var highlightColor = getHighlightColor()
+    var wordElem = generateWordElem(word, highlightColor)
+    var button = event.currentTarget
+    var container = button.parentNode.parentNode
+    var wordsContainer = container.getElementsByClassName("words-container")[0]
+    wordsContainer.appendChild(wordElem)
+    counter++
+  }
+}
+
+var addWordButtons = document.getElementsByClassName("add-word-button")
+for (var i = 0; i < addWordButtons.length; i++) {
+  addWordButtons[i].addEventListener("click", handleAddWordButtonClick)
 }
 
 /*
